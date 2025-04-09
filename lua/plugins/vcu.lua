d7627f5ed6a26933c446fe142a06ff57b6a178a7
gingerab <singleplayer176@gmail.com>
2025-04-09 21:19:49 +0200

    small changes

--- lua/plugins/vcu.lua
+++ lua/plugins/vcu.lua
@@ -23,12 +23,12 @@ return{
                 return "<C-k>"
             end
         end, { silent = true })
-        -- vim.keymap.set({ "i", "s" }, "<Tab>", function()
-        --     ls.jump(1)
-        -- end, { silent = true })
-        -- vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
-        --     ls.jump(-1)
-        -- end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<Tab>", function()
+            ls.jump(1)
+        end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+            ls.jump(-1)
+        end, { silent = true })
         end,
     }
 }
