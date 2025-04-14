481983beb7b013284a8d5826de8131f0c8ae2592
gingerab <singleplayer176@gmail.com>
2025-04-14 17:51:09 +0200

    small changes

--- lua/plugins/vcu.lua
+++ lua/plugins/vcu.lua
@@ -4,17 +4,17 @@ return{
         dependencies = { "L3MON4D3/LuaSnip", "nvim-treesitter/nvim-treesitter" },
         config = function()
         require("madol").setup()
-        local ls = require("luasnip")
-        ls.config.setup({
-            enable_autosnippets = true,
-            -- store_selection_keys = "<Tab>",
-        })
-        vim.keymap.set({ "s", "i" }, "<C-j>", function()
-            if ls.choice_active() then
-                ls.change_choice(1)
-            else
-                return "<C-j>"
-            end
+            local ls = require("luasnip")
+            ls.config.setup({
+                enable_autosnippets = true,
+                store_selection_keys = "<Tab>",
+            })
+            vim.keymap.set({ "s", "i" }, "<C-j>", function()
+                if ls.choice_active() then
+                    ls.change_choice(1)
+                else
+                    return "<C-j>"
+                end
         end, { silent = true })
         vim.keymap.set({ "s", "i" }, "<C-k>", function()
             if ls.choice_active() then
