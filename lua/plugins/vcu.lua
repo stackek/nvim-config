9ed7d56eea39496e96387dc9e1a57f2c890c7e97
gingerab <singleplayer176@gmail.com>
2025-04-15 19:32:05 +0200

    added custom snippets to madol.nvim

--- lua/plugins/vcu.lua
+++ lua/plugins/vcu.lua
@@ -1,14 +1,54 @@
 return{
     {
-        dir = '/home/gwegus/dev/nvim/vcus',
+	'https://gitlab.com/repetitivesin/madol.nvim',
         dependencies = { "L3MON4D3/LuaSnip", "nvim-treesitter/nvim-treesitter" },
         config = function()
-        require("madol").setup()
-            local ls = require("luasnip")
-            ls.config.setup({
-                enable_autosnippets = true,
-                store_selection_keys = "<Tab>",
-            })
+        local ls = require("luasnip")
+        ls.config.setup({
+            enable_autosnippets = true,
+            store_selection_keys = "<Tab>",
+        })
+
+        function my_markdown_snippets(is_math, not_math)
+          local snippets = {}
+          local t = ls.text_node
+          local i = ls.insert_node
+          local _local_1_ = require("luasnip.extras.fmt") local fmta = _local_1_["fmta"]
+          local ss = ls.extend_decorator.apply(ls.snippet, {condition = not_math, snippetType = "autosnippet"})
+
+          local function s(...) return table.insert(snippets, ss(...)) end
+          s({trig = "cmm", name = "Caution Block"}, fmta(
+            "::: {.caution title=\"<>\" ref=\"<>\"}\n<>\n:::",
+            {i(1), i(2), i(3)}
+          ))
+
+          s({trig = "tmm", name = "Theorem Block"}, fmta(
+            "::: {.theorem title=\"<>\" ref=\"<>\"}\n<>\n:::",
+            {i(1), i(2), i(3)}
+          ))
+
+          s({trig = "emm", name = "Example Block"}, fmta(
+            "::: {.example title=\"<>\" ref=\"<>\"}\n<>\n:::",
+            {i(1), i(2), i(3)}
+          ))
+
+          s({trig = "lmm", name = "Lemma Block"}, fmta(
+            "::: {.lemma title=\"<>\" ref=\"<>\"}\n<>\n:::",
+            {i(1), i(2), i(3)}
+          ))
+           return snippets
+        end
+
+        require("madol").setup({
+            markdown = {
+                snippets = {
+                  -- Reminder: without brackets, my_markdown_snippets would be treated as
+                  -- string
+                  [my_markdown_snippets] = true,
+                },
+              },
+        })
+
             vim.keymap.set({ "s", "i" }, "<C-j>", function()
                 if ls.choice_active() then
                     ls.change_choice(1)
